26aa5181837efff9de392b89cb5f7806ca34afb6
Aurélien Lorence <aurelien@keyframe.fr>
2023-11-16 10:28:50 +0100

    Articles Routes / Controller / Base view

---
+++ src/Controller/PostController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use App\Core\Abstracts\AbstractController;
+
+class PostController extends AbstractController
+{
+
+    public function index()
+    {
+        return $this->render('post/index.html.twig');
+    }
+
+    public function show()
+    {
+        return $this->render('post/show.html.twig');
+    }
+}
