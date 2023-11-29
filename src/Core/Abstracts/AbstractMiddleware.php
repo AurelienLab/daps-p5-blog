714f034d4259f56f4d32b52b8247ffbd80af6bd8
Aurélien Lorence <aurelien@keyframe.fr>
2023-11-29 11:51:15 +0100

    Fix #10 - Create AbstractMiddleware

---
+++ src/Core/Abstracts/AbstractMiddleware.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Core\Abstracts;
+
+use Symfony\Component\HttpFoundation\Request;
+
+abstract class AbstractMiddleware
+{
+
+    /**
+     * Content of the middleware
+     *
+     * @param Request $request
+     *
+     * @return void
+     */
+    abstract public function handle(Request $request): void;
+}
