760072a7db287d9eb9743d69161ea9f8a8098e04
Aurélien Lorence <aurelien@keyframe.fr>
2024-01-17 15:33:12 +0100

    Fix #43 User structure

---
+++ src/Model/User.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace App\Model;
+
+class User
+{
+
+    const TABLE = 'users';
+
+    private int $id;
+    private string $name;
+    private string $email;
+    private string $password;
+    private ?string $rememberMeToken;
+
+    private bool $isAdmin = false;
+
+    private string $profilePicture;
+
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    public function setId(int $id): User
+    {
+        $this->id = $id;
+        return $this;
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): User
+    {
+        $this->name = $name;
+        return $this;
+    }
+
+    public function getEmail(): string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): User
+    {
+        $this->email = $email;
+        return $this;
+    }
+
+    public function getPassword(): string
+    {
+        return $this->password;
+    }
+
+    public function setPassword(string $password): User
+    {
+        $this->password = $password;
+        return $this;
+    }
+
+    public function getRememberMeToken(): ?string
+    {
+        return $this->rememberMeToken;
+    }
+
+    public function setRememberMeToken(?string $rememberMeToken): User
+    {
+        $this->rememberMeToken = $rememberMeToken;
+        return $this;
+    }
+
+    public function isAdmin(): bool
+    {
+        return $this->isAdmin;
+    }
+
+    public function setIsAdmin(bool $isAdmin): User
+    {
+        $this->isAdmin = $isAdmin;
+        return $this;
+    }
+
+    public function getProfilePicture(): string
+    {
+        return $this->profilePicture;
+    }
+
+    public function setProfilePicture(string $profilePicture): User
+    {
+        $this->profilePicture = $profilePicture;
+        return $this;
+    }
+
+
+}
---
+++ src/Repository/UserRepository.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Repository;
+
+use App\Core\Abstracts\AbstractRepository;
+use App\Model\User;
+
+class UserRepository extends AbstractRepository
+{
+
+    const MODEL = User::class;
+}
